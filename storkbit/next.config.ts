c87cea936219500e4bc01d4646e008053721a6a3
Josh Storkey <joshs93@hotmail.com>
2025-09-02 18:31:37 +0000

    Initial Storkbit layout

--- storkbit/next.config.ts
+++ storkbit/next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
+import { PHASE_DEVELOPMENT_SERVER } from 'next/constants'
+ 
+const isDev = process.env.NODE_ENV === 'development'
 
 const nextConfig: NextConfig = {
+  assetPrefix: isDev ?  '/proxy/3000' : undefined,
   /* config options here */
 };
 
 export default nextConfig;
+/** 
+  //Consider using dev/prod config files
+
+  import type { NextConfig } from 'next'
+ 
+const isProd = process.env.NODE_ENV === 'production'
+ 
+const nextConfig: NextConfig = {
+  typescript: {
+    tsconfigPath: isProd ? 'tsconfig.build.json' : 'tsconfig.json',
+  },
+}
+ 
+export default nextConfig
+
+*/
---
+++ storkbit/src/app/components/Home/home.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import "./home.css"
+
+
+const  Home = () => {
+  return (
+    <Container className="home tc">
+      <div className="navy tc">
+        <p className="title tc animate__animated animate__zoomInDown">
+          <span>Hi, my name is <span className="name">Joshua Storkey</span></span>
+        </p>
+        <p className="subTitle tc animate__animated animate__zoomInDown">
+          <span>I&apos;m the <span className="profession">Lead Developer</span> at Storkbit</span>
+        </p>
+      </div>
+    </Container>
+  );
+}
+
+export default Home
