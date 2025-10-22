885fa79fc43a12456906d324e55132185ae99c72
Josh Storkey <joshs93@hotmail.com>
2025-10-22 15:26:20 +0100

    Update icon layout, add tooltips, adjust padding

--- storkbit/src/app/components/Links/links.tsx
+++ storkbit/src/app/components/Links/links.tsx
@@ -3,39 +3,46 @@ import Container from 'react-bootstrap/Container';
 import Link from "next/link";
 import { BsGithub, BsLinkedin } from "react-icons/bs";
 import { MdEmail, MdPhone } from "react-icons/md";
-import { SiLeetcode } from "react-icons/si";
 import { IoDocumentText } from "react-icons/io5";
 import { personalData } from "./../../personal-data";
+import Tooltip from '@mui/material/Tooltip';
 import "./links.css"
 
 
 const Links = () => {
     return (
         <Container className="links tc">
-          <a>
-            
-            <Link href={personalData.resume}>
-              <IoDocumentText size={50} className="icon"/>
-            </Link>
-            <Link
-              href={personalData.linkedIn}
-              target='_blank'
-            >
-              <BsLinkedin size={50} className="icon"/>
-            </Link>
-            <Link href={'mailto:' + personalData.email}>
-              <MdEmail size={50} className="icon"/>
-            </Link>
-            <Link href={'tel:' + personalData.phone}>
-              <MdPhone size={50} className="icon"/>
-            </Link>
-            <Link
-              href={personalData.github}
-              target='_blank'
-            >
-              <BsGithub size={50} className="icon"/>
-            </Link>
-          </a>
+
+            <Tooltip title="CV">
+              <Link href={personalData.resume}>
+                <IoDocumentText size={50} className="icon"/>
+              </Link>
+            </Tooltip>
+            <Tooltip title="LinkedIn">
+              <Link
+                href={personalData.linkedIn}
+                target='_blank'>
+                <BsLinkedin size={50} className="icon"/>
+              </Link>
+            </Tooltip>
+            <Tooltip title="Email">
+              <Link href={'mailto:' + personalData.email}>
+                <MdEmail size={50} className="icon"/>
+              </Link>
+            </Tooltip>
+            <Tooltip title="Mobile">
+              <Link href={'tel:' + personalData.phone}>
+                <MdPhone size={50} className="icon"/>
+              </Link>
+            </Tooltip>
+            <Tooltip title="GitHub Website Code">
+              <Link
+                href={personalData.github}
+                target='_blank'
+              >
+                <BsGithub size={50} className="icon"/>
+              </Link>
+            </Tooltip>
         </Container>
     );
 }
